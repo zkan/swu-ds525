e97ebf8b30db6e870da922ad41d2b58e84ccc305
Kan Ouivirach <kan@odds.team>
2022-08-25 16:14:42 +0000

    Connect and insert data to Cassandra

--- 02-data-modeling-ii/etl.py
+++ 02-data-modeling-ii/etl.py
@@ -1,63 +1,104 @@
 from cassandra.cluster import Cluster
 
 
-cluster = Cluster(['127.0.0.1'])
-session = cluster.connect()
+table_drop = "DROP TABLE events"
 
-# Create keyspace
-try:
-    session.execute(
-        """
-        CREATE KEYSPACE IF NOT EXISTS github_events
-        WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }
-        """
+table_create = """
+    CREATE TABLE IF NOT EXISTS events
+    (
+        id text,
+        type text,
+        public boolean,
+        PRIMARY KEY (
+            id,
+            type
+        )
     )
-except Exception as e:
-    print(e)
-
-# Set keyspace
-try:
-    session.set_keyspace("github_events")
-except Exception as e:
-    print(e)
-
-query = """
-CREATE TABLE IF NOT EXISTS table_name
-(
-    x int,
-    y int,
-    z text,
-    PRIMARY KEY (
-        x,
-        y
-    )
-)
 """
-try:
+
+create_table_queries = [
+    table_create,
+]
+drop_table_queries = [
+    table_drop,
+]
+
+def drop_tables(session):
+    for query in drop_table_queries:
+        try:
+            rows = session.execute(query)
+        except Exception as e:
+            print(e)
+
+
+def create_tables(session):
+    for query in create_table_queries:
+        try:
+            session.execute(query)
+        except Exception as e:
+            print(e)
+
+
+def process(session, filepath):
+    # Get list of files from filepath
+    all_files = get_files(filepath)
+
+    for datafile in all_files:
+        with open(datafile, "r") as f:
+            data = json.loads(f.read())
+            for each in data:
+                # Print some sample data
+                print(each["id"], each["type"], each["actor"]["login"])
+
+                # Insert data into tables here
+
+
+def insert_sample_data(session):
+    query = f"""
+    INSERT INTO events (id, type, public) VALUES ('23487929637', 'IssueCommentEvent', true)
+    """
     session.execute(query)
-except Exception as e:
-    print(e)
 
 
-# Read files then insert data into table
-query = """
-INSERT INTO table_name (x, y, z) VALUES (%s, %s, %s)
-"""
-session.execute(query, 1, 2, 3)
+def main():
+    cluster = Cluster(['127.0.0.1'])
+    session = cluster.connect()
+
+    # Create keyspace
+    try:
+        session.execute(
+            """
+            CREATE KEYSPACE IF NOT EXISTS github_events
+            WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }
+            """
+        )
+    except Exception as e:
+        print(e)
+
+    # Set keyspace
+    try:
+        session.set_keyspace("github_events")
+    except Exception as e:
+        print(e)
+
+    drop_tables(session)
+    create_tables(session)
+
+    # process(session, filepath="../data")
+    insert_sample_data(session)
+
+    # Select data in Cassandra and print them to stdout
+    query = """
+    SELECT * from events WHERE id = '23487929637' AND type = 'IssueCommentEvent'
+    """
+    try:
+        rows = session.execute(query)
+    except Exception as e:
+        print(e)
+
+    for row in rows:
+        print(row)
 
 
-query = "SELECT xyz from table_name WHERE x = 1 AND y = 2"
-try:
-    rows = session.execute(query)
-except Exception as e:
-    print(e)
-
-for row in rows:
-    print(row)
-
-# Drop table
-query = "drop table table_name"
-try:
-    rows = session.execute(query)
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    main()
